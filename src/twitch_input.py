204e63308a80894cf790a7b12dbadbf3996eda8b
Jordan-Lane <jlane@ualberta.ca>
2018-01-27 23:51:02 -0700

    Polling from url works

--- src/twitch_input.py
+++ src/twitch_input.py
@@ -3,14 +3,14 @@
 import rospy
 import urllib
 
-#from rover_pkg.msg import input_msg
+from rover_pkg.msg import input_msg
 
 
 class TwitchInputNode():
   def __init__(self):
   
     # Initialize the publisher
-#    pub = rospy.Publisher('twitch_topic', input_msg)
+    pub = rospy.Publisher('twitch_topic', input_msg)
 
     # Create a rate object
     rate = rospy.Rate(2) # 2Hz
@@ -18,12 +18,16 @@ class TwitchInputNode():
     while not rospy.is_shutdown():
       # Get message from url
       req = urllib.urlopen("http://ec2-52-53-222-168.us-west-1.compute.amazonaws.com:8080")
-      msg = req.read()
+      twitch_str = req.read()
+
+      msg = input_msg()
+      msg.x_coord = ''
+      msg.y_coord = ''
 
       rospy.loginfo(msg)
 
       # Publish message to topic
-#      pub.publish(msg)
+      pub.publish(msg)
 
       # Goodnight my sweet prince
       rate.sleep()
